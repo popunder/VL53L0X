617be92eb4778d0d4fed774e5a0c572f4c59eacc
popunder <pv.zakharov@gmail.com>
2016-06-10 16:47:54 +0200

    reads out, reports status of measurements

--- VL53L0Xtest.py
+++ VL53L0Xtest.py
@@ -2,8 +2,6 @@ import smbus
 import struct
 import time
 
-'''Function with parameter.'''
-
 def bswap(val):
     return struct.unpack('<H', struct.pack('>H', val))[0]
 def mread_word_data(adr, reg):
@@ -12,13 +10,28 @@ def mwrite_word_data(adr, reg, data):
     return bus.write_word_data(adr, reg, bswap(data))
 def makeuint16(lsb, msb):
     return ((msb & 0xFF) << 8)  | (lsb & 0xFF)
+def VL53L0X_decode_vcsel_period(vcsel_period_reg):
+# Converts the encoded VCSEL period register value into the real
+# period in PLL clocks
+    vcsel_period_pclks = (vcsel_period_reg + 1) << 1;
+    return vcsel_period_pclks;
 
-VL53L0X_REG_IDENTIFICATION_MODEL_ID           = 0x00c0
-VL53L0X_REG_IDENTIFICATION_REVISION_ID        = 0x00c2
-VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD     = 0x0050
-VL53L0X_REG_SYSRANGE_START = 0x000
-bus = smbus.SMBus(1)
+
+
+VL53L0X_REG_IDENTIFICATION_MODEL_ID		= 0x00c0
+VL53L0X_REG_IDENTIFICATION_REVISION_ID		= 0x00c2
+VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD	= 0x0050
+VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD	= 0x0070
+VL53L0X_REG_SYSRANGE_START			= 0x000
+
+VL53L0X_REG_RESULT_INTERRUPT_STATUS 		= 0x0013
+VL53L0X_REG_RESULT_RANGE_STATUS 		= 0x0014
+
+
 address = 0x29
+
+bus = smbus.SMBus(1)
+
 val1 = bus.read_byte_data(address, VL53L0X_REG_IDENTIFICATION_REVISION_ID)
 print "Revision ID: " + hex(val1)
 val1 = bus.read_byte_data(address, VL53L0X_REG_IDENTIFICATION_MODEL_ID)
@@ -27,6 +40,8 @@ print "Device ID: " + hex(val1)
 #		Status = VL53L0X_RdByte(Dev,
 #			VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD,
 #			&vcsel_period_reg);
+val1 = bus.read_byte_data(address, VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD)
+print "PRE_RANGE_CONFIG_VCSEL_PERIOD=" + hex(val1) + " decode: " + str(VL53L0X_decode_vcsel_period(val1))
 
 
 #	case VL53L0X_VCSEL_PERIOD_FINAL_RANGE:
@@ -34,11 +49,34 @@ print "Device ID: " + hex(val1)
 #			VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD,
 #			&vcsel_period_reg);
 
+val1 = bus.read_byte_data(address, VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD)
+print "FINAL_RANGE_CONFIG_VCSEL_PERIOD=" + hex(val1) + " decode: " + str(VL53L0X_decode_vcsel_period(val1))
 
 #		Status = VL53L0X_WrByte(Dev, VL53L0X_REG_SYSRANGE_START, 0x01);
 val1 = bus.write_byte_data(address, VL53L0X_REG_SYSRANGE_START, 0x01)
+
+#		Status = VL53L0X_RdByte(Dev, VL53L0X_REG_RESULT_RANGE_STATUS,
+#			&SysRangeStatusRegister);
+#		if (Status == VL53L0X_ERROR_NONE) {
+#			if (SysRangeStatusRegister & 0x01)
+#				*pMeasurementDataReady = 1;
+#			else
+#				*pMeasurementDataReady = 0;
+#		}
+cnt = 0
+while (cnt < 100): # 1 second waiting time max
+	time.sleep(0.010)
+	val = bus.read_byte_data(address, VL53L0X_REG_RESULT_RANGE_STATUS)
+	if (val & 0x01):
+		break
+	cnt += 1
+
+if (val & 0x01):
+	print "ready"
+else:
+	print "not ready"
+
 #	Status = VL53L0X_ReadMulti(Dev, 0x14, localBuffer, 12);
-time.sleep(0.200)
 data = bus.read_i2c_block_data(address, 0x14, 12)
 print data
 print "ambient count " + str(makeuint16(data[7], data[6]))
